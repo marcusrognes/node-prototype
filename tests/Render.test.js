8c93c22ff32fc8e522e44be6c2027435b82e92bd
Marcus Rognes <marcus@rognes.guru>
2019-03-30 12:24:40 +0100

    Small test tweaks

--- tests/Render.test.js
+++ tests/Render.test.js
@@ -2,15 +2,13 @@ require('dotenv').config();
 
 const { StartServer } = require('../dist/server/server');
 
-var server = null;
+let server = null;
 const PORT = process.env.PORT || 4000;
 const ROOT_URL = `http://localhost:${PORT}`;
 
 describe('Server with react', () => {
 	beforeAll(async () => {
 		({ server } = await StartServer());
-
-		await page.goto(`${ROOT_URL}`);
 	});
 
 	afterAll(async () => {
@@ -18,6 +16,8 @@ describe('Server with react', () => {
 	}, 3000);
 
 	it('should display "Login" text on page', async () => {
+		await page.goto(`${ROOT_URL}`);
+
 		await expect(page).toMatch('Login');
 	});
 });
